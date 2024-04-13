a6f1043e0d857544c5ed72d9c93143bd52e79d95
蔣永寬 <mrzackchiang@gmail.com>
2024-04-13 12:47:08 +0800

    store message to elasticsearch when text messaging

--- teamSpark/src/main/java/org/example/teamspark/controller/TextMessageController.java
+++ teamSpark/src/main/java/org/example/teamspark/controller/TextMessageController.java
@@ -4,6 +4,7 @@ import lombok.extern.apachecommons.CommonsLog;
 import org.example.teamspark.model.InGroupMessage;
 import org.example.teamspark.model.InMessage;
 import org.example.teamspark.model.OutMessage;
+import org.example.teamspark.service.ElasticsearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -16,23 +17,47 @@ public class TextMessageController {
     @Autowired
     private SimpMessagingTemplate messageTemplate;
 
+    @Autowired
+    private ElasticsearchService elasticsearchService;
+
+    private static String concatenateAndSort(String str1, String str2) {
+        String result = str1.compareTo(str2) < 0 ? str1 + "_" + str2 : str2 + "_" + str1;
+        return result.toLowerCase();
+    }
+
     @MessageMapping("/groupTextChat")
     public void handleGroupMessage(InGroupMessage message) throws Exception {
+
+        OutMessage outMessage = new OutMessage(message.getFrom() + " send: " + message.getContent());
+        outMessage.setFrom(message.getFrom());
+        // store message to elasticsearch
+        elasticsearchService.addMessageToIndex(message.getChatGroup().toLowerCase(), outMessage);
+
         messageTemplate.convertAndSend(
                 "/textChatGroup/" + message.getChatGroup(),
-                new OutMessage(message.getFrom() + " send: " + message.getContent()));
+                outMessage);
     }
 
     @MessageMapping("/privateTextChat")
     public void handlePrivateMessage(InMessage message) {
+
+        OutMessage outMessage = new OutMessage();
+        outMessage.setFrom(message.getFrom());
+        outMessage.setContent(message.getFrom() + " send: " + message.getContent());
+
+        // store message to elasticsearch
+        elasticsearchService.addMessageToIndex(
+                concatenateAndSort(message.getFrom(), message.getTo()),
+                outMessage);
+
         // Send to Message Receiver
         messageTemplate.convertAndSend(
                 "/privateTextChat/" + message.getTo(),
-                new OutMessage(message.getFrom() + " send: " + message.getContent()));
+                outMessage);
 
         // Send to Message Sender
         messageTemplate.convertAndSend(
                 "/privateTextChat/" + message.getFrom(),
-                new OutMessage(message.getFrom() + " send: " + message.getContent()));
+                outMessage);
     }
 }
