4e99fd010bd57b9584dd4bfb80b7101da27c486d
蔣永寬 <mrzackchiang@gmail.com>
2024-04-25 08:29:34 +0800

    [feat] login to workspace textmessaging userflow connect to api
    - since user can not yet join workspace, and channel member add ui hasn't set up, causing FE error.

--- teamSpark/src/main/java/org/example/teamspark/controller/frontEnd/FrontController.java
+++ teamSpark/src/main/java/org/example/teamspark/controller/frontEnd/FrontController.java
@@ -1,9 +1,7 @@
 package org.example.teamspark.controller.frontEnd;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class FrontController {
@@ -18,16 +16,18 @@ public class FrontController {
         return "loginPage";
     }
 
-    @GetMapping("/user/{userId}/workspace/{workspaceId}")
-    public String workspacePage(@PathVariable String userId, @PathVariable String workspaceId, Model model) {
-        model.addAttribute("user_id", userId);
-        model.addAttribute("workspace_id", userId);
+    @GetMapping("/user")
+    public String userPage() {
+        return "user";
+    }
+
+    @GetMapping("/workspace/{workspaceId}")
+    public String workspacePage() {
         return "textMessaging";
     }
 
     @GetMapping("/channel/{channelId}/videoCall")
-    public String workspacePage(@PathVariable String channelId, Model model) {
-        model.addAttribute("channel_id", channelId);
+    public String channelVideoCallPage() {
         return "videoCall";
     }
 }
